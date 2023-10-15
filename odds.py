203c361122fe5f907fd1c5d3e5a00e7228ca707d
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-10-15 12:44:22 -0400

    update docs

--- odds.py
+++ odds.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.openapi.models import Info
+from fastapi.openapi.models import ExternalDocumentation
 import requests
 from dotenv import load_dotenv
 import os
@@ -7,7 +9,35 @@ import uvicorn
 # Load environment variables from the .env file
 load_dotenv()
 
-app = FastAPI()
+app = FastAPI(
+    title="Sports Odds API",
+    description="An API to retrieve sports odds data.",
+    version="1.0.0",
+    openapi_tags=[
+        {
+            "name": "Sports",
+            "description": "Endpoints related to sports data.",
+        }
+    ],
+    info=Info(
+        title="Sports Odds API",
+        version="1.0.0",
+        description="An API to retrieve sports odds data.",
+        terms_of_service="https://sports-odds.onrender.com/terms",
+        contact={
+            "name": "Matt Majestic",
+            "url": "https://www.youtube.com/@majesticcoding",
+        },
+        license={
+            "name": "Apache 2.0",
+            "url": "https://www.apache.org/licenses/LICENSE-2.0",
+        },
+    ),
+    external_docs=ExternalDocumentation(
+        description="Find more information here",
+        url="https://sports-odds.onrender.com/docs",
+    ),
+)
 
 @app.get("/")
 async def get_sports():
@@ -22,5 +52,5 @@ async def get_sports():
     else:
         return {"error": "Failed to fetch data from the Odds API"}
 
-if __name__ == "__odds__":
+if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8885)
